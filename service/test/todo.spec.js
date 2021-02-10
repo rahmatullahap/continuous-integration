f99ed119c01f77e205b090570d1bfbf9b768b56f
Rahmatullah Arrizal <rahmatullaharrizal@gmail.com>
2021-02-11 02:54:50 +0700

    nock, node-fecth

--- service/test/todo.spec.js
+++ service/test/todo.spec.js
@@ -2,7 +2,10 @@
 const { connect } = require('../lib/orm');
 const { TodoSchema } = require('../todo.model');
 const { config } = require('../config');
-// const server = require('../server');
+const server = require('../server');
+const fetch = require('node-fetch');
+
+const nock = require('nock');
 
 const { list, truncate, add } = require('../todo');
 
@@ -10,7 +13,7 @@ describe('todos', () => {
   let connection;
   beforeAll(async () => {
     connection = await connect([TodoSchema], config.database, false);
-    // server.run();
+    server.run();
   });
   beforeEach(async () => {
     await truncate();
@@ -23,13 +26,50 @@ describe('todos', () => {
   afterAll(async () => {
     await truncate();
     await connection.close();
-    // server.stop();
+    server.stop();
   });
 
   describe('list', () => {
-    it('get list', async () => {
+    it.skip('get list', async () => {
       const todos = await list();
       expect(todos).toHaveLength(1);
+    });
+    it('get list by fetch', async () => {
+      const mutation = await fetch('http://localhost:7767/add', {
+        method: 'post',
+        body: JSON.stringify({
+          task: 'task 2',
+          done: false,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const res = await mutation.json();
+      console.log(res);
+      expect(res.task).toBe('task 2');
+
+      // intercept with nock
+      nock('http://localhost:7767')
+        .get('/list')
+        .reply(200, [
+          {
+            id: 1,
+            task: 'buy the milk',
+            done: false,
+          },
+          {
+            id: 2,
+            task: 'buy the tea',
+            done: false,
+          },
+        ]);
+
+      const response = await fetch('http://localhost:7767/list', {
+        method: 'get',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data = await response.json();
+      console.log(data);
+      expect(data).toHaveLength(2);
     });
   });
 });
