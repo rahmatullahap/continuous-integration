63bcc8341023c087ad92f302025e6f7a2be0090c
Rahmatullah Arrizal <rahmatullaharrizal@gmail.com>
2021-02-11 13:26:50 +0700

    config

--- service/config.js
+++ service/config.js
@@ -6,11 +6,11 @@ const defaultConfig = {
     host: 'localhost',
     port: 5432,
     username: 'postgres',
-    password: 'postgres',
-    database: 'sanbercode',
+    password: '',
+    database: 'database',
   },
   server: {
-    port: 7767,
+    port: 80,
   },
 };
 
